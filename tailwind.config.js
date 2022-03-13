c63cf3ef49a83a98203b2043c8be5321f3e5d1a4
João Amadeu <jmamadeu2000@gmail.com>
2022-03-13 01:52:52 +0100

    chore: add colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,30 @@ module.exports = {
       fontFamily: {
         headline: ['Tienne'],
         default: ['Roboto']
+      },
+      colors: {
+        darkPurple: {
+          100: '#130F1C',
+          200: '#56447F',
+          300: '#8E82A9'
+        },
+        gray: {
+          0: '#000000',
+          10: '#1F1B29',
+          25: '#2E2A36',
+          50: '#5A5761',
+          100: '#5A5761',
+          200: '#BAB9BD',
+          300: '#DEDEE0',
+          400: '#ECECEE',
+          500: '#F3F3F5',
+          600: '#FAFAFA'
+        },
+        purple: {
+          100: '#651FFF',
+          200: '#4C17BF',
+          300: '#331080'
+        }
       }
     },
   },

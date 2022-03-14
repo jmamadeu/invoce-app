d4bce2fba8a8b4e4df8ceac6dade436fd3304044
João Amadeu <jmamadeu2000@gmail.com>
2022-03-14 02:16:36 +0100

    chore: add form input component

---
+++ src/components/ui/form-input/index.tsx
@@ -0,0 +1,30 @@
+import { ComponentPropsWithRef } from "react";
+
+type FormInputProperties = ComponentPropsWithRef<"input"> & {
+  label?: string;
+  name: string;
+  type: string;
+  placeholder?: string;
+};
+
+export const FormInput = ({
+  label,
+  name,
+  type,
+  placeholder,
+  ...rest
+}: FormInputProperties) => (
+  <div className="flex flex-col gap-1">
+    <label htmlFor={name} className="text-gray-200 text-xs font-normal">
+      {label}
+    </label>
+    <input
+      className="bg-gray-25 py-3 pl-3 rounded-md placeholder:text-gray-50 placeholder:text-sm text-gray-200 text-sm"
+      id={name}
+      name={name}
+      type={type}
+      placeholder={placeholder}
+      {...rest}
+    />
+  </div>
+);

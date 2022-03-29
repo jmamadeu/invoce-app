32c2f0101f438b31c1880592786787951636824d
João Amadeu <jmamadeu2000@gmail.com>
2022-03-29 22:30:48 +0100

    chore: add validations

--- src/pages/setup-my-company.tsx
+++ src/pages/setup-my-company.tsx
@@ -1,7 +1,7 @@
 import type { GetServerSideProps, NextPage } from "next";
 import React from "react";
 
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { FormInput } from "@/components/ui";
 import { PageWrapper } from "@/components/layouts/page-wrapper";
@@ -11,16 +11,27 @@ import { CompanyDetailsType } from "@/models/user/types";
 import { setupMyCompanyFormSchemaValidation } from "@/views/setup-my-component/utils";
 
 const SetupMyCompany: NextPage = () => {
-  const { register } = useForm<CompanyDetailsType>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<CompanyDetailsType>({
     resolver: yupResolver(setupMyCompanyFormSchemaValidation)
   });
 
+  const onSubmit: SubmitHandler<CompanyDetailsType> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <PageWrapper pageTitle="Setup my company">
-      <main className="bg-darkPurple-100 h-screen flex flex-col">
+    <PageWrapper className="bg-darkPurple-100" pageTitle="Setup my company">
+      <main className="h-screen flex flex-col">
         <Header />
         <div className="container mx-auto flex justify-center items-center mt-8">
-          <form className="bg-gray-10 rounded-xl p-4 py-12 gap">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="bg-gray-10 rounded-xl p-4 py-12 gap"
+          >
             <h3 className="text-gray-200 font-headline mb-4 text-2xl text-center">
               Setup company details
             </h3>
@@ -30,12 +41,14 @@ const SetupMyCompany: NextPage = () => {
                 placeholder="Toptal"
                 label="Insert your company's name"
                 register={register("name")}
+                error={errors.name?.message}
               />
               <FormInput
                 type="text"
                 placeholder="San Francisco"
                 label="Company's address"
                 register={register("address")}
+                error={errors.address?.message}
               />
 
               <div className="flex gap-4 flex-row">
@@ -44,12 +57,14 @@ const SetupMyCompany: NextPage = () => {
                   placeholder="1223"
                   label="Insert your vat number"
                   register={register("vatNumber")}
+                  error={errors.vatNumber?.message}
                 />
                 <FormInput
                   type="text"
                   placeholder="1223"
                   label="Insert your reg number"
                   register={register("regNumber")}
+                  error={errors.regNumber?.message}
                 />
               </div>
               <div className="flex gap-4 flex-row">
@@ -58,12 +73,14 @@ const SetupMyCompany: NextPage = () => {
                   placeholder="BE32432934893"
                   label="Insert your Iban"
                   register={register("iban")}
+                  error={errors.iban?.message}
                 />
                 <FormInput
                   type="text"
                   placeholder="TOP123"
                   label="Insert your Swift"
                   register={register("swift")}
+                  error={errors.swift?.message}
                 />
               </div>
             </div>

246f84dbcb59cc275d14a0e7fabf45b1eb8529ad
João Amadeu <jmamadeu2000@gmail.com>
2022-04-20 10:56:11 +0100

    chore: add navigation to anoter pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import type { GetServerSideProps, NextPage } from "next";
 
+import { useRouter } from "next/router";
 import { Header } from "@/components/layouts/header";
 import { PageWrapper } from "@/components/layouts/page-wrapper";
 import { DashboardClientsTable } from "@/views/dashboard/components/table";
@@ -9,6 +10,12 @@ import { SectionTableHeader } from "@/views/dashboard/components/section-table-h
 import { withAuth } from "@/guards/with-ssr-auth";
 
 const Home: NextPage = () => {
+  const { push: navigateToRoute } = useRouter();
+
+  const handleClickCreateEntity = (entity: string) => {
+    navigateToRoute(entity);
+  };
+
   return (
     <PageWrapper pageTitle="Dashboard">
       <main className="bg-darkPurple-100 h-screen">
@@ -18,7 +25,7 @@ const Home: NextPage = () => {
             <aside>
               <SectionTableHeader
                 label="Clients"
-                onButtonClick={() => console.log("")}
+                onButtonClick={() => handleClickCreateEntity("/clients/save")}
                 title="button to add new client"
               />
               <div className="mt-8">
@@ -28,7 +35,7 @@ const Home: NextPage = () => {
             <aside>
               <SectionTableHeader
                 label="Invoices"
-                onButtonClick={() => console.log("")}
+                onButtonClick={() => handleClickCreateEntity("/invoices/save")}
                 title="button to add new invoice"
               />
             </aside>
